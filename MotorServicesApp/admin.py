2436ba5242c6d5dc7ba812ed112df7922effb61e
MIS-Web Developer-Shakil Ahammad <shakil.ahammad@aci-bd.local>
2019-09-17 21:41:29 +0600

    Dashboard done

--- MotorServicesApp/admin.py
+++ MotorServicesApp/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group, User
-from .models import UserManager, Status, AreaNew
+from django import forms
+#from django.contrib.auth.models import Group, User
+from .models import  Area,Territory, MotorTechnician, Target, ServiceDetails, UserInfo
 
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+
+admin.site.site_header = "Motor Service Administration"
+admin.site.site_title = "Motor Service"
+admin.site.index_title = "Motor Service"
+
+
+admin.site.register(Area)
+admin.site.unregister(User)
 
 class UserCreateForm(UserCreationForm):
 
@@ -23,34 +33,134 @@ class UserAdmin(UserAdmin):
             'fields': ('first_name', 'last_name', 'username', 'password1', 'password2', 'groups','is_staff' ),
         }),
     )
-admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
+class TerritoryAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Code')
+    search_fields = ['Name']
+    list_filter = ['Name', 'Code']
+    ordering = ['-Id']
+    # readonly_fields = ['ahref_tag']
+    list_per_page = 20
+
+    def get_queryset(self, request):
+        qs = super(TerritoryAdmin, self).get_queryset(request)
+        return qs.filter(user=request.user)
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("user",)
+        kwargs['widgets'] = {'Notes': forms.Textarea}
+        form = super(TerritoryAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super(TerritoryAdmin, self).save_model(request, obj, form, change)
 
 
-# Register your models here.
-
-admin.site.site_header = 'Motor Service Administration'
-admin.site.site_title = "Motor Service"
-admin.site.index_title = "Motor Service"
-
-# class UserManagerAdmin(admin.ModelAdmin):
-#     list_display = [ 'UserName', 'Password', 'AccessLevel', 'Status', 'DisplayName']
-#     search_fields = [ 'UserName', 'DisplayName']
-#     list_filter = ['DisplayName', 'UserName']
-#     list_per_page = 20
-#
-#     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-#         if db_field.name == "AccessLevel":
-#             kwargs["queryset"] = AreaNew.objects.all()
-#         if db_field.name == "Status":
-#             kwargs["queryset"] = Status.objects.all()
-#         return super().formfield_for_foreignkey(db_field, request, **kwargs)
-#
-# admin.site.register(UserManager, UserManagerAdmin)
-
-#admin.site.unregister(Group)
-#admin.site.unregister(User)
+admin.site.register(Territory, TerritoryAdmin)
 
 
+class MotorTechnicianAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Designation','StaffId', 'TerritoryCode','MobileNo')
+    search_fields = ['Name', 'StaffId', 'MobileNo']
+    list_filter = ['StaffId', 'MobileNo']
+    ordering = ['-Id']
+    # readonly_fields = ['ahref_tag']
+    list_per_page = 20
+
+    def get_queryset(self, request):
+        qs = super(MotorTechnicianAdmin, self).get_queryset(request)
+        return qs.filter(user=request.user)
+
+    def save_model(self, request, obj, form, change):
+        UserInfo(UserName=str(obj.StaffId), Password=str(obj.StaffId), IsActive=1, user=request.user).save()
+        obj.user = request.user
+        return super(MotorTechnicianAdmin, self).save_model(request, obj, form, change)
+
+    # def save_model(self, request, obj, form, change):
+    #     obj.user = request.user
+    #     return super(MotorTechnicianAdmin, self).save_model(request, obj, form, change)
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("user",)
+        #kwargs['widgets'] = {'Notes': forms.Textarea}
+        form = super(MotorTechnicianAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+admin.site.register(MotorTechnician, MotorTechnicianAdmin)
+
+
+class TargetAdmin(admin.ModelAdmin):
+    list_display = ('TechnicianCode', 'TerritoryId', 'WarrantyService', 'PostWarrantyService', 'EntryDate')
+    search_fields = ['TechnicianCode', 'TerritoryId']
+    list_filter = ['TechnicianCode', 'EntryDate']
+    ordering = ['-Id']
+    list_per_page = 20
+
+    def get_queryset(self, request):
+        qs = super(TargetAdmin, self).get_queryset(request)
+        return qs.filter(user=request.user)
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("user",)
+        form = super(TargetAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "TechnicianCode":
+            kwargs["queryset"] = MotorTechnician.objects.filter(user=request.user)
+        if db_field.name == "TerritoryId":
+            kwargs["queryset"] = Territory.objects.filter(user=request.user)
+
+        return super(TargetAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super(TargetAdmin, self).save_model(request, obj, form, change)
+
+admin.site.register(Target, TargetAdmin)
+
+class ServiceDetailsAdmin(admin.ModelAdmin):
+    list_display = ('CustomerName', 'Mobile', 'HoursProvided', 'CategoryId', 'ProductId', 'CallTypeId')
+    #list_display = ('CustomerName', 'Mobile')
+    search_fields = ['CustomerName', 'Mobile']
+    list_filter = ['IsVerify','CustomerName', 'Mobile']
+    #readonly_fields = ('CustomerName',)
+    ordering = ['-ServiceDetailsId']
+    list_per_page = 20
+    fields = ('CustomerName'
+                ,'Mobile'
+                ,'TractorPurchaseDate'
+                ,'HoursProvided'
+                ,'DateOfInstallation'
+                ,'ServiceDemandDate'
+                ,'ServiceStartDate'
+                ,'ServiceEndDate'
+                ,'ServiceIncome'
+                ,'VisitDate'
+                ,'UserId'
+                ,'CategoryId'
+                ,'ProductId'
+                ,'CallTypeId'
+                ,'IsVerify')
+
+
+
+    def get_queryset(self, request):
+        qs = super(ServiceDetailsAdmin, self).get_queryset(request)
+        return qs.filter(SupervisorCode=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "UserId":
+            kwargs["queryset"] = UserInfo.objects.filter(UserName__in=(MotorTechnician.objects.filter(user=request.user).values('StaffId')))
+        # if db_field.name == "TerritoryId":
+        #     kwargs["queryset"] = ServiceDetails.objects.filter(SupervisorCode=request.user)
+        return super(ServiceDetailsAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = ("SupervisorCode",)
+        return super(ServiceDetailsAdmin, self).get_form(request, obj, **kwargs)
+
+admin.site.register(ServiceDetails, ServiceDetailsAdmin)
 
--- MotorServicesApp/models.py
+++ MotorServicesApp/models.py
@@ -2,83 +2,267 @@ from django.db import models
 
 # Create your models here.
 
-class Status(models.Model):
-    Id = models.AutoField(primary_key=True, db_column='Id')
-    Name = models.CharField(max_length=50, db_column='Name')
+
+from django.contrib.auth.models import Group, User
+
+
+# class Region(models.Model):
+#     Id = models.AutoField(primary_key=True, db_column='RegionId')
+#     RegionName = models.CharField(max_length=100, db_column='RegionName')
+#     PartId = models.IntegerField(db_column='PartId', default=0)
+#     Notes = models.CharField(max_length=100, db_column='Notes')
+#     #user = models.ForeignKey(User)
+#
+#     def __str__(self):
+#         return self.RegionName
+#
+#     class Meta:
+#         managed = True
+#         db_table = 'Region'
+
+class Area(models.Model):
+    Id = models.AutoField(primary_key=True, db_column='AreaId')
+    AreaName = models.CharField(max_length=100, db_column='AreaName',unique=True)
+    Notes = models.CharField(max_length=100, db_column='Notes')
+
+    # RegionId = models.ForeignKey(Region, db_column='RegionId', on_delete=models.CASCADE)
+    #user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.AreaName
+
+    class Meta:
+        managed = True
+        db_table = 'Area'
+
+class Territory(models.Model):
+    Id = models.AutoField(primary_key=True, db_column='TerritoryId')
+    Name = models.CharField(max_length=100, db_column='TerritoryName')
+    Code = models.CharField(max_length=50, db_column='TerritoryCode')
+    Notes = models.CharField(max_length=100, db_column='Notes')
+    user = models.ForeignKey(User,db_column='EntryBy',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.Name
 
     class Meta:
-        managed = False
-        db_table = 'Status'
+        managed = True
+        db_table = 'Territory'
 
-class AreaNew(models.Model):
-    Id = models.AutoField(primary_key=True, db_column='AreaID')
-    Name = models.CharField(max_length=100, db_column='AreaName')
+class MotorTechnician(models.Model):
+    Id = models.AutoField(primary_key=True, db_column='TechnicianId')
+    Name = models.CharField(max_length=100, db_column='TechnicianName', default='')
+    Designation = models.CharField(max_length=100, db_column='Designation')
+    StaffId = models.CharField(max_length=100, db_column='StaffId', default='',unique=True)
+    TerritoryCode = models.ForeignKey(Territory, db_column='TerritoryCode', on_delete=models.CASCADE)
+    MobileNo = models.CharField(max_length=20, db_column='MobileNo', default='')
+    BloodGroup = models.CharField(max_length=20, db_column='BloodGroup')
+    Notes = models.CharField(max_length=100, db_column='Notes')
+    user = models.ForeignKey(User, db_column='SupervisorCode', on_delete=models.CASCADE, default=1)
+
+    # added_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.Name
 
     class Meta:
-        managed = False
-        db_table = 'AreaNew'
+        managed = True
+        db_table = 'MotorTechnician'
 
-class UserManager(models.Model):
-    UserID = models.AutoField(primary_key=True, db_column='UserID')
-    UserName = models.CharField(max_length=255, db_column='UserName', unique=True)
-    Password = models.CharField(max_length=255, db_column='Password')        
-    Status =  models.ForeignKey(Status, db_column='Status', on_delete=models.CASCADE)
-    AccessLevel = models.ForeignKey(AreaNew, db_column='AccessLevel', on_delete=models.CASCADE)
-    DisplayName = models.CharField(max_length=100, db_column='DisplayName')
+class Target(models.Model):
+    Id = models.AutoField(primary_key=True, db_column='TargetId')
+    TechnicianCode = models.ForeignKey(MotorTechnician, db_column='TsaTsoStaffID', on_delete=models.CASCADE)
+    TerritoryId = models.ForeignKey(Territory, db_column='TerritoryId', on_delete=models.CASCADE)
+    WarrantyService = models.IntegerField(db_column='WarrantyService', default=0)
+    PostWarrantyService = models.IntegerField(db_column='PostWarrantyService', default=0)
+    Notes = models.CharField(max_length=100, db_column='Notes')
+    EntryDate = models.DateTimeField(auto_now_add=True, db_column='EntryDate')
+    user = models.ForeignKey(User, db_column='SupervisorCode', on_delete=models.CASCADE)
 
     def __str__(self):
-       return "Username: " + self.UserName + " has been created successfully."
+        return self.TechnicianCode.Name
 
-    def get_full_name(self):
+    class Meta:
+        managed = True
+        db_table = 'Target'
+
+class UserInfo(models.Model):
+    UserId = models.AutoField(primary_key=True)
+    UserName = models.CharField(max_length=50)
+    Password = models.CharField(max_length=50)
+    IsActive = models.IntegerField(default=1, db_column='IsActive')
+    user = models.ForeignKey(User, db_column='SupervisorID', on_delete=models.CASCADE)
+
+    def __str__(self):
         return self.UserName
 
-    def get_full_Status(self):
-        return self.Status.Name
-
-    def get_full_UserType(self):
-        return self.UserType.Name
-
-    def get_full_AccessLevel(self):
-        return self.AccessLevel.Name
-
     class Meta:
         managed = False
-        db_table = 'UserManager'
+        db_table = 'GN_UserInfo'
 
-class TerritoryNew(models.Model):
-    TerritoryID = models.AutoField(primary_key=True, db_column='TerritoryID')
-    TerritoryCode = models.CharField(max_length=50, db_column='TerritoryCode')
-    TerritoryName = models.CharField(max_length=100, db_column='TerritoryName')
-    AreaName =  models.ForeignKey(AreaNew, db_column='AreaID', on_delete=models.CASCADE)
-    Notes = models.CharField(max_length=100, db_column='Notes')
+    # def __str__(self):
+    #     return '{}, {}'.format(self.UserName, self.Password)
+
+class ServiceCategory(models.Model):
+    CategoryId = models.AutoField(primary_key=True)
+    CategoryDetails = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.TerritoryName
+        return self.CategoryDetails
 
     class Meta:
         managed = False
-        db_table = 'TerritoryNew'
+        db_table = 'GN_ServiceCategory'
 
-class Technician(models.Model):
-    Id = models.AutoField(primary_key=True, db_column='TechnicianID')
-    TechnicianName = models.CharField(max_length=50, db_column='TechnicianName')
-    Designation = models.CharField(max_length=100, db_column='Designation')
-    StaffID = models.CharField(max_length=100, db_column='StaffID')
-    MobileNo = models.CharField(max_length=100, db_column='MobileNo')
-    BloodGroup = models.CharField(max_length=100, db_column='BloodGroup')
-    TerritoryCode = models.CharField(max_length=100, db_column='TerritoryCode')
-    Notes = models.CharField(max_length=100, db_column='Notes')
-    EntryBy =  models.ForeignKey(UserManager, db_column='SupervisorCode', on_delete=models.CASCADE)
+    # def __str__(self):
+    #     return '{}, {}'.format(self.CategoryDetails)
+
+class Product(models.Model):
+    ProductId = models.AutoField(primary_key=True)
+    ProductName = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.TechnicianName
+        return self.ProductName
 
     class Meta:
         managed = False
-        db_table = 'Technician'
+        db_table = 'GN_Product'
+
+    # def __str__(self):
+    #     return '{}, {}'.format(self.ProductName)
+
+
+class ServiceCall(models.Model):
+    CallTypeId = models.AutoField(primary_key=True)
+    CallTypeDetails = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.CallTypeDetails
+
+    class Meta:
+        managed = False
+        db_table = 'GN_ServiceCall'
+
+    # def __str__(self):
+    #     return '{}, {}'.format(self.CallTypeDetails)
+
+
+class ServiceDetails(models.Model):
+    ServiceDetailsId = models.AutoField(primary_key=True)
+    CustomerName = models.CharField(max_length=100)
+    Mobile = models.CharField(max_length=20)
+    TractorPurchaseDate = models.DateTimeField()
+    HoursProvided = models.IntegerField()
+    DateOfInstallation = models.DateTimeField()
+    ServiceDemandDate = models.DateTimeField()
+    ServiceStartDate = models.DateTimeField()
+    ServiceEndDate = models.DateTimeField()
+    ServiceIncome = models.FloatField()
+    VisitDate = models.DateTimeField()
+    MobileCreatedDT = models.DateTimeField()
+    MobileEditedDT = models.DateTimeField()
+    MobileLogCount = models.IntegerField()
+    MobileId = models.IntegerField()
+    ServerInsertDateTime = models.DateTimeField(auto_now_add=True)
+    ServerUpdateDateTime = models.DateTimeField(auto_now=True)
+
+    IsVerify = models.BooleanField(db_column='IsVerify', default=False)
+
+    #UserId = models.ForeignKey(UserInfo, db_column='UserId', on_delete=models.CASCADE)
+    UserId = models.ForeignKey(UserInfo, db_column='UserId', on_delete=models.CASCADE)
+    CategoryId = models.ForeignKey(ServiceCategory, db_column='CategoryId', on_delete=models.CASCADE)
+    ProductId = models.ForeignKey(Product, db_column='ProductId', on_delete=models.CASCADE)
+    CallTypeId = models.ForeignKey(ServiceCall, db_column='CallTypeId', on_delete=models.CASCADE)
+    SupervisorCode = models.ForeignKey(User, db_column='SupervisorCode', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.CustomerName
+
+    class Meta:
+        managed = False
+        db_table = 'GN_ServiceDetails'
+
+    # def __str__(self):
+    #     return '{}'.format( self.CustomerName)
+
+
+# class Status(models.Model):
+#     Id = models.AutoField(primary_key=True, db_column='Id')
+#     Name = models.CharField(max_length=50, db_column='Name')
+#
+#     def __str__(self):
+#         return self.Name
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'Status'
+#
+# class AreaNew(models.Model):
+#     Id = models.AutoField(primary_key=True, db_column='AreaID')
+#     Name = models.CharField(max_length=100, db_column='AreaName')
+#
+#     def __str__(self):
+#         return self.Name
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'AreaNew'
+#
+# class UserManager(models.Model):
+#     UserID = models.AutoField(primary_key=True, db_column='UserID')
+#     UserName = models.CharField(max_length=255, db_column='UserName', unique=True)
+#     Password = models.CharField(max_length=255, db_column='Password')
+#     Status =  models.ForeignKey(Status, db_column='Status', on_delete=models.CASCADE)
+#     AccessLevel = models.ForeignKey(AreaNew, db_column='AccessLevel', on_delete=models.CASCADE)
+#     DisplayName = models.CharField(max_length=100, db_column='DisplayName')
+#
+#     def __str__(self):
+#        return "Username: " + self.UserName + " has been created successfully."
+#
+#     def get_full_name(self):
+#         return self.UserName
+#
+#     def get_full_Status(self):
+#         return self.Status.Name
+#
+#     def get_full_UserType(self):
+#         return self.UserType.Name
+#
+#     def get_full_AccessLevel(self):
+#         return self.AccessLevel.Name
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'UserManager'
+#
+# class TerritoryNew(models.Model):
+#     TerritoryID = models.AutoField(primary_key=True, db_column='TerritoryID')
+#     TerritoryCode = models.CharField(max_length=50, db_column='TerritoryCode')
+#     TerritoryName = models.CharField(max_length=100, db_column='TerritoryName')
+#     AreaName =  models.ForeignKey(AreaNew, db_column='AreaID', on_delete=models.CASCADE)
+#     Notes = models.CharField(max_length=100, db_column='Notes')
+#
+#     def __str__(self):
+#         return self.TerritoryName
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'TerritoryNew'
+#
+# class Technician(models.Model):
+#     Id = models.AutoField(primary_key=True, db_column='TechnicianID')
+#     TechnicianName = models.CharField(max_length=50, db_column='TechnicianName')
+#     Designation = models.CharField(max_length=100, db_column='Designation')
+#     StaffID = models.CharField(max_length=100, db_column='StaffID')
+#     MobileNo = models.CharField(max_length=100, db_column='MobileNo')
+#     BloodGroup = models.CharField(max_length=100, db_column='BloodGroup')
+#     TerritoryCode = models.CharField(max_length=100, db_column='TerritoryCode')
+#     Notes = models.CharField(max_length=100, db_column='Notes')
+#     EntryBy =  models.ForeignKey(UserManager, db_column='SupervisorCode', on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.TechnicianName
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'Technician'
